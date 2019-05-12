6b7bd37a89793c65c4a88eeeff0ba526af400625
VincentTran <vincent.tran@bento.cloud>
2019-05-12 22:06:03 +0800

    add feature to switch build while running, this along with build syncing will create a lot of potential for dynamic strategy

--- sc2ba.py
+++ sc2ba.py
@@ -20,7 +20,7 @@ import sys
 
 BuildStep = namedtuple('BuildStep', ['supply', 'sync', 'time', 'message'])
 
-lotv_regex = re.compile(r'([+\-\d]+)\|?(\w*)\t\s*(\d+:\d+)\t\s*([\w ,]+)')
+lotv_regex = re.compile(r'([+\-\d]+)\|?(\w*\+?)\s*\t?\s*(\d+:\d+)\t\s*([\w ,]+)')
 
 
 def parse_build(build_content, factor=1, verbose=0, max_time=60 * 15):
@@ -30,7 +30,7 @@ def parse_build(build_content, factor=1, verbose=0, max_time=60 * 15):
         sync_keys = match[1]
         m, s = match[2].split(':')
         btime = datetime.timedelta(minutes=int(m), seconds=int(s)).seconds
-        msg = match[3]
+        msg = match[3].strip()
         step = BuildStep(supply, sync_keys, btime / factor, msg)
         if verbose:
             print(step)
@@ -74,6 +74,7 @@ def say(msg):
 
 
 class Runner(object):
+    build_path = ""
     build_name = ""
     build_orders = []
     cur_second = 0
@@ -85,58 +86,71 @@ class Runner(object):
 
 
 def run_build(run, start_key='', max_time=60 * 15):
-    keyboard.call_later(say, args=['build %s is ready' % run.build_name], delay=0.1)
-    print("build is ready", run.build_name)
     if start_key:
         print("Press {} to start".format(start_key))
         keyboard.wait(start_key)
     runner.stop_now = False
     keyboard.call_later(say, args=['start'], delay=0)
     start_time = datetime.datetime.now()
-    cur_second = 0
-    last_step = None
-    while cur_second <= max_time:
-        cur_second = (datetime.datetime.now() - start_time).seconds
-        run.cur_second = cur_second
-        second = cur_second + run.offset
+    run.cur_second = 0
+    run.last_step = None
+    second = 0
+    while second <= max_time:
+        run.cur_second = (datetime.datetime.now() - start_time).seconds
+        second = run.cur_second + run.offset
         step = find_build_step(second, run.build_orders)
-        if step != last_step:
+        if step != run.last_step:
             keyboard.call_later(say, args=[step.message], delay=0)
-            print(cur_second, run.offset, step)
-            last_step = step
+            print(run.cur_second, run.offset, step)
             run.last_step = step
         if run.stop_now or step == run.build_orders[-1]:
             # final step
-            return
+            break
         time.sleep(0.1)
-    keyboard.call_later(say, args=['build %s is stop' % run.build_name], delay=0.1)
+    keyboard.call_later(say, args=['build is stop'], delay=0.1)
+    print("build is stop")
 
 
 def process_runner_build_orders(run, enable_sync=True):
+    # remove all sync handler
+    if run.sync_handler_map:
+        print("remove all existing sync handler")
+        for handler in run.sync_handler_map.values():
+            try:
+                keyboard.remove_word_listener(handler)
+            except KeyError:
+                pass
+
     for step in run.build_orders:
         if step.sync and len(step.sync) >= 2 and enable_sync:
-            print("create sync for:", step)
+
+            if step.sync[-1] == '+':
+                keys = step.sync[:-1]  # remove char + out of keys sync
+                _rmv_handler = None
+            else:
+                keys = step.sync
+                _rmv_handler = str(keys)
+
+            print("create sync %s for:" % (keys), step)
 
             def make_sync_build():
-                step_time, keys = step.time, step.sync  # duplicated the value to avoid referrence
-
-                def f(step_time=step_time):
+                def f(step_time=step.time, remove_handler_key=_rmv_handler):
                     print("sync build", run.cur_second, step_time)
                     if run.cur_second > step_time:  # only sync backward (go back in time)
                         run.offset = step_time - run.cur_second  # offset will be negative
-                        # remove remove sync listerner
-                        handler = run.sync_handler_map.pop(keys, None)
-                        if handler:
-                            keyboard.remove_word_listener(handler)
-                            print("build synced and handler removed")
+                        if remove_handler_key is not None:  # remove remove sync listerner
+                            handler = run.sync_handler_map.pop(remove_handler_key, None)
+                            if handler:
+                                keyboard.remove_word_listener(handler)
+                                print("build synced and handler removed")
 
                 return f
 
-            run.sync_handler_map[str(step.sync)] = keyboard.add_word_listener(str(step.sync[:-1]),
-                                                                              make_sync_build(),
-                                                                              triggers=[str(step.sync[-1])],
-                                                                              match_suffix=True,
-                                                                              timeout=0.5)
+            run.sync_handler_map[str(keys)] = keyboard.add_word_listener(str(keys),
+                                                                         make_sync_build(),
+                                                                         triggers=[str(keys[-1])],
+                                                                         match_suffix=True,
+                                                                         timeout=0.6)
 
 
 def print_test(kb_event, *args):
@@ -146,16 +160,39 @@ def print_test(kb_event, *args):
 def get_build_path(verbose=0):
     build_folder_path = os.path.join(os.getcwd(), 'build')
     build_list = sorted([p for p in os.listdir(build_folder_path) if p.endswith('.txt')])
+    build_path_list = [os.path.join(build_folder_path, build_path) for build_path in build_list]
     build_index = None
     if len(sys.argv) >= 2:
         build_index = (int(sys.argv[1]) - 1) % len(build_list)
-        file_path = os.path.join(build_folder_path, build_list[build_index])
+        file_path = build_path_list[build_index]
     else:
-        file_path = 'build/PvZ_DT_Drop_Into_Archon.txt'
-    build_name = os.path.split(file_path)[-1].replace('.txt', '')
+        file_path = build_path_list[-1]  # default is last one 'build/PvZ_DT_Drop_Into_Archon.txt'
+
     if verbose:
-        print("Build index: %s, path: %s" % (build_index, file_path))
-    return file_path, build_name
+        print("Build index: %s, build path: %s" % (build_index, file_path))
+    return file_path, build_path_list
+
+
+def reload_runner(set_offset=1, verbose='say'):
+    global runner
+    runner.build_name = os.path.split(runner.build_path)[-1].replace('.txt', '').replace('_', ' ')
+    with open(runner.build_path) as f:
+        bo = parse_build(f.read(), verbose=0, factor=1)
+    runner.build_orders = bo
+    if set_offset is not None:
+        runner.offset = set_offset
+
+    # enable sync feature
+    process_runner_build_orders(runner, enable_sync=True)
+    # build is now ready
+    if verbose and 'say' in verbose:
+        m = re.search(r'say (.+)', verbose)
+        if m:
+            msg = m.group(1)
+        else:
+            msg = 'build %s is ready' % runner.build_name
+        keyboard.call_later(say, args=[msg], delay=0)
+    print("build is ready", runner.build_name)
 
 
 def main():
@@ -164,31 +201,33 @@ def main():
     global runner
     runner = Runner()
 
-    file_path, build_name = get_build_path(verbose=1)
-    with open(file_path) as f:
-        bo = parse_build(f.read(), verbose=0, factor=1)
-    # for s in bo:
-    #     print s
-    runner.build_name = build_name.replace('_', ' ')
-    runner.build_orders = bo
-    runner.offset = 1
-
-    # enable sync feature
-    process_runner_build_orders(runner, enable_sync=True)
+    runner.build_path, build_path_list = get_build_path(verbose=0)
 
     # test code
     # keyboard.hook(print_test, suppress=False)
     # keyboard.wait('command')
 
-    def stop_now(*args):
+    def stop_now():
         runner.stop_now = True
-        with open(file_path) as f:
-            bo = parse_build(f.read())
-            runner.build_orders = bo
-            print("stop and reload build")
+        print("stop build now")
 
-    keyboard.add_word_listener('stop', stop_now, triggers=['space'], match_suffix=True, timeout=1)
+    keyboard.add_word_listener('stop', stop_now, triggers=['space'], match_suffix=True, timeout=1.2)
+
+    for i in range(len(build_path_list)):
+        build_index = i + 1
+
+        def make_switch_build_func():
+            def f(build_path=str(build_path_list[i]), _build_index=build_index):
+                runner.build_path = build_path
+                reload_runner(set_offset=None, verbose='say build %d is ready' % (_build_index))
+
+            return f
+
+        keyboard.add_word_listener('b' + str(build_index), make_switch_build_func(), triggers=['space'],
+                                   match_suffix=True, timeout=1.2)
+
     while 1:
+        reload_runner()
         run_build(runner, start_key='q')
 
 
